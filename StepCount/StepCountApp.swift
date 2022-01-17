5b0e336ebfdd1184e9149cbb411134a5cd58c05b
coderluka <iluka.dimitrijevic@gmail.com>
2022-01-17 14:13:33 +0100

    Initial Commit

---
+++ StepCount/StepCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCountApp.swift
+//  StepCount
+//
+//  Created by Luka Dimitrijević on 17.01.22.
+//
+
+import SwiftUI
+
+@main
+struct StepCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
